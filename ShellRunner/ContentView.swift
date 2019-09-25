8052398d467d5f69b6ea7b635210ea73762348a0
poberWong <poberwong@gmail.com>
2019-09-25 12:17:58 +0800

    Initial Commit

---
+++ ShellRunner/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ShellRunner
+//
+//  Created by Pober Wong on 2019/9/25.
+//  Copyright © 2019 Pober Wong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
